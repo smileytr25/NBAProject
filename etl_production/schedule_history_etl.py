334fb1c48d1b9fb7cdf19af4afd3c569da3e7b0b
smileytr25 <smileytr@msu.edu>
2026-06-25 22:56:01 -0700

    resolved bug fixes to schedule history etl

--- etl_production/schedule_history_etl.py
+++ etl_production/schedule_history_etl.py
@@ -4,6 +4,7 @@ import numpy as np
 import time 
 from pathlib import Path 
 from urllib.error import HTTPError
+import requests 
 
 def get_month_game_schedule(month, year):
     url = f"https://www.basketball-reference.com/leagues/{"NBA" if year >= 1950 else "BAA"}_{year}_games-{month}.html"
@@ -39,7 +40,7 @@ def get_month_game_schedule(month, year):
 
         if "Start_Time" in schedule.columns.tolist() and schedule["Start_Time"].isna().sum():
             schedule = schedule.drop(columns=["Start_Time"])
-            
+
         if "Start_Time" in schedule.columns.tolist():
             s = schedule["Start_Time"].str.strip().str.lower()
             s = s.str.replace(r"(\d)(a)$", r"\1AM", regex=True)
@@ -49,13 +50,14 @@ def get_month_game_schedule(month, year):
         
         print(f"Schedule history added for {month}, {year}")
         return schedule 
-    except HTTPError:
-        print(f"Schedule history added for {month}, {year}")
+    except HTTPError as e:
+        print(f"Schedule history finished with error for url: {url}, {month}, {year}")
+        print(e)
         return pd.DataFrame()
 
 def get_year_game_schedule(year, page_limit, pages_visited=0, start_time=None):
     months = [
-        "ocotober", "november", "december", "january", "february", "march", 
+        "october", "november", "december", "january", "february", "march", 
         "april", "may", "june"
     ]
 
@@ -103,15 +105,67 @@ def move_game_schedule_to_database(schedule):
 
     print("Successfully moved to database.")
 
-def check_for_schedules_to_scrape():
+def check_for_schedules_to_scrape(page_limit=15):
     db_path = Path("~/Personal Project/data/nba.db").expanduser()
     engine = create_engine(f"sqlite:///{db_path}")
 
     with engine.connect() as conn:
         query = text("SELECT MAX(date) FROM game_schedule_history")
-        last_day_in_db = conn.execute(query).fetchone()[0]
+        last_date_in_db = pd.to_datetime(conn.execute(query).fetchone()[0])
+        next_date_to_check = last_date_in_db + pd.DateOffset(months=1)
 
-        start_of_new_days = next_day_to_check = last_day_in_db + pd.Timedelta(days=1)
+        dates_available = [] 
+        more_dates = True 
+
+        pages_visited = 0
+        start_time = time.time()
+
+        while more_dates:
+            next_month_to_check, next_year_to_check = pd.to_datetime(next_date_to_check).dt.month_name.str.lower(), int(pd.to_datetime(next_date_to_check).dt.year)
+            next_season_year = next_year_to_check if next_month_to_check in ["january", "february", "march", "april", "may", "june"] else next_year_to_check + 1
+            
+            if pages_visited == page_limit:
+                wait_time = max(0, 60 - (time.time() - start_time))
+                print(f"Rate limited. Waiting for {wait_time:.2f} seconds")
+                time.sleep(wait_time)
+                pages_visited = 0
+                start_time = time.time()
+            
+            next_month_status_code = requests.get(f"https://www.basketball-reference.com/leagues/NBA_{next_year_to_check}_games-{next_month_to_check}.html")
+            pages_visited += 1
+
+            if pages_visited == page_limit:
+                wait_time = max(0, 60 - (time.time() - start_time))
+                print(f"Rate limited. Waiting for {wait_time:.2f} seconds")
+                time.sleep(wait_time)
+                pages_visited = 0
+                start_time = time.time()
+
+            next_season_status_code = requests.get(f"https://www.basketball-reference.com/leagues/NBA_{next_season_year}_games-october.html")
+            pages_visited += 1
+
+            if next_month_status_code != 200 and next_season_status_code != 200: 
+                more_dates = False 
+
+            elif next_month_status_code == 200:
+                dates_available.append(next_year_to_check)
+                next_date_to_check = next_date_to_check + pd.DateOffset(months=1)
+            
+            elif next_season_status_code == 200:
+                next_date_to_check = pd.Timestamp(year=next_season_year, month=10, day=1)
+
+        dates_available = list(set(dates_available))
+        return dates_available
+
+def get_schedules_not_already_existing(years):
+    db_path = Path("~/Personal Project/data/nba.db").expanduser()
+    engine = create_engine(f"sqlite:///{db_path}")
+
+    years_existing = []
+    with engine.connect() as conn:
+        years_existing += [year[0] for year in conn.execute(text("SELECT DISTINCT strftime('%Y', Date) FROM game_schedule_history")).fetchall()]
+
+    return [year for year in years if year not in years_existing]
 
 def game_schedule_history_etl(years, page_limit):
     if years:
@@ -122,4 +176,4 @@ def game_schedule_history_etl(years, page_limit):
         print("All years are accounted for.")
 
 if __name__ == "__main__":
-    game_schedule_history_etl(list(range(1947, 2027)), 19)
+    game_schedule_history_etl(list(range(1947, 2027)), 15)
